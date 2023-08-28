ffddc6a7295f3b0a8f0444398dc7a5807c81b894
ewillwin <jw3418@naver.com>
2023-08-28 22:08:05 +0900

    [level 2] Title: 주식가격, Time: 77.66 ms, Memory: 17 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -0,0 +1,17 @@
+def solution(prices):
+    N = len(prices)
+    
+    result = []
+    
+    for i in range(N-1):
+        
+        cnt = 0
+        for j in range(i, N-1):
+            if prices[i] <= prices[j]:
+                cnt += 1
+            else:
+                break
+        result.append(cnt)
+    result.append(0)
+            
+    return result
