9b95cb26c0e534759a886376da01f79931f012c7
ewillwin <jw3418@naver.com>
2023-08-29 22:21:01 +0900

    Update Квадратный корень.py

--- 백준/Bronze/29623. Квадратный корень/Квадратный корень.py
+++ 백준/Bronze/29623. Квадратный корень/Квадратный корень.py
@@ -1,18 +1,14 @@
 from decimal import Decimal, getcontext
 
-n = int(input())
-
 getcontext().prec = 30
 
+n = int(input())
 for _ in range(n):
     a, b, c, d = map(int, input().split())
     
     l = Decimal(a) + Decimal(b) ** Decimal('0.5')
     r = Decimal(c) + Decimal(d) ** Decimal('0.5')
     
-    if l < r:
-        print("Less")
-    elif l == r:
-        print("Equal")
-    else:
-        print("Greater")
+    if l < r: print("Less")
+    elif l == r: print("Equal")
+    else: print("Greater")
