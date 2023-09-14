cc398a2115d09436ae3e6c750353c68a62159e53
ewillwin <jw3418@naver.com>
2023-09-15 00:56:59 +0900

    Update 리코쳇 로봇.cpp

--- 프로그래머스/unrated/169199. 리코쳇 로봇/리코쳇 로봇.cpp
+++ 프로그래머스/unrated/169199. 리코쳇 로봇/리코쳇 로봇.cpp
@@ -1,74 +1,76 @@
 #include <string>
 #include <vector>
 #include <queue>
+#include <limits.h>
 #define MAX 100
 
 using namespace std;
 
-int n, m;
-int answer = 2147000000;
+int N, M;
+int answer = INT_MAX;
 pair<int, int> start;
 pair<int, int> goal;
-bool visited[MAX][MAX];
+bool visited[MAX][MAX]; // 전역변수 초기화 -> False
 
 int dx[4] = {-1, 1, 0, 0};
 int dy[4] = {0, 0, -1, 1};
 
 void bfs(vector<string> &board) {
     queue<pair<pair<int, int>, int>> q;
-    
     q.push({{start.first, start.second}, 0});
     visited[start.first][start.second] = true;
     
     while (!q.empty()) {
-        int cx = q.front().first.first;
-        int cy = q.front().first.second;
-        int cnt = q.front().second;
+        int x = q.front().first.first;
+        int y = q.front().first.second;
+        int depth = q.front().second;
         q.pop();
         
-        if (cx == goal.first && cy == goal.second) {
-            answer = min(answer, cnt);
+        if (x == goal.first && y == goal.second) {
+            answer = min(answer, depth);
         }
         
         for (int i=0; i<4; i++) {
-            int nx = cx + dx[i];
-            int ny = cy + dy[i];
+            int nx = x + dx[i];
+            int ny = y + dy[i];
             
-            if ((nx < 0 || n <= nx || ny < 0 || m <= ny) || board[nx][ny] == 'D') continue;
+            if (0 <= nx && nx < N && 0 <= ny && ny < M){
+                if (board[nx][ny] != 'D'){
+                    while (true) {
+                        nx += dx[i];
+                        ny += dy[i];
+                    
+                        if ((nx < 0 || N <= nx || ny < 0 || M <= ny) || board[nx][ny] == 'D') {
+                            nx -= dx[i];
+                            ny -= dy[i];
+                            break;
+                        }
+                    }
+           
+                    if (visited[nx][ny]) continue;
             
-            while (true) {
-                nx += dx[i];
-                ny += dy[i];
-                
-                if ((nx < 0 || n <= nx || ny < 0 || m <= ny) || board[nx][ny] == 'D') {
-                    nx -= dx[i];
-                    ny -= dy[i];
-                    break;
+                    q.push({{nx, ny}, depth + 1});
+                    visited[nx][ny] = true;
                 }
             }
-           
-            if (visited[nx][ny]) continue;
-            
-            q.push({{nx, ny}, cnt + 1});
-            visited[nx][ny] = true;
         }
     }
 }
 
 int solution(vector<string> board) {
-    n = board.size();
-    m = board[0].size();
+    N = board.size();
+    M = board[0].size();
     
-    int findCnt = 0;
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<m; j++) {
-            if (findCnt == 2) break;
+    int cnt = 0;
+    for (int i=0; i<N; i++) {
+        for (int j=0; j<M; j++) {
+            if (cnt == 2) break;
             
             if (board[i][j] == 'R') {
-                findCnt++;
+                cnt++;
                 start = {i, j};
             } else if (board[i][j] == 'G') {
-                findCnt++;
+                cnt++;
                 goal = {i, j};
             }
         }
@@ -76,6 +78,6 @@ int solution(vector<string> board) {
     
     bfs(board);
     
-    if (answer == 2147000000) return -1;
+    if (answer == INT_MAX) return -1;
     return answer;
 }
