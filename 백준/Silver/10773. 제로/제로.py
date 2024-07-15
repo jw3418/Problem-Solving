62fbf5879fa004f695c3fb97cab6fcee57e9741a
ewillwin <jw3418@naver.com>
2024-07-15 21:21:24 +0900

    [Silver IV] Title: 제로, Time: 112 ms, Memory: 114236 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -4,6 +4,7 @@ input = sys.stdin.readline
 K = int(input())
 stack = []
 for k in range(K):
-    stack.append(int(input()))
-    if stack[-1] == 0: stack.pop(); stack.pop()
+    n = int(input())
+    if n == 0: stack.pop()
+    else: stack.append(n)
 print(sum(stack))
