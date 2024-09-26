e899eff86ed796ef232eed8cac8786fa466ca9f8
ewillwin <jw3418@naver.com>
2024-09-26 21:39:04 +0900

    [Gold III] Title: 내리막 길, Time: 112 ms, Memory: 44056 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.py
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -1,29 +1,29 @@
 import sys
+sys.setrecursionlimit(10**8)
+input = sys.stdin.readline
 
 dx = (0, 0, -1, 1)
 dy = (-1, 1, 0, 0)
 
+M, N = map(int, input().split())
+board = []
+for m in range(M): board.append(list(map(int, input().split())))
+
 def dfs(x, y):
     if (x, y) == (M-1, N-1):
         return 1
-
-    if dp[x][y] != -1:
+    elif dp[x][y] != -1:
         return dp[x][y]
 
     cnt = 0
     for i in range(4):
-        nx = x + dx[i]; ny = y + dy[i]
-        if 0 <= nx < M and 0 <= ny < N:
+        nx, ny = x+dx[i], y+dy[i]
+        if 0<=nx<M and 0<=ny<N:
             if board[x][y] > board[nx][ny]:
                 cnt += dfs(nx, ny)
     dp[x][y] = cnt
-
     return dp[x][y]
-    
-M, N = map(int, sys.stdin.readline()[:-1].split())
-board = []
-for m in range(M):
-    board.append(list(map(int, sys.stdin.readline()[:-1].split())))
 
-dp = [[-1] * N for _ in range(M)]
-print(dfs(0, 0))
+dp = [[-1]*N for m in range(M)]
+dfs(0, 0)
+print(dp[0][0])
