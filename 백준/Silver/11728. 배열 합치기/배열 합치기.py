da989c98897eeb4bef1ea02ffafdeb12b469673f
ewillwin <jw3418@naver.com>
2025-01-21 01:05:10 +0900

    [Silver V] Title: 배열 합치기, Time: 740 ms, Memory: 338748 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.py
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.py
@@ -3,12 +3,14 @@ input = sys.stdin.readline
 
 N, M = map(int, input().split())
 A = list(map(int, input().split()))
-B = list(map(int, input().split())); B.sort(reverse=True)
+B = list(map(int, input().split())); # B.sort(reverse=True)
+res = A+B; res.sort()
+print(" ".join(map(str, res)))
 
-result = []
-for n in range(N):
-    while B and B[-1] < A[n]:
-        result.append(B.pop())
-    result.append(A[n])
-while B: result.append(B.pop())
-print(" ".join(map(str, result)))
+# result = []
+# for n in range(N):
+#     while B and B[-1] < A[n]:
+#         result.append(B.pop())
+#     result.append(A[n])
+# while B: result.append(B.pop())
+# print(" ".join(map(str, result)))
