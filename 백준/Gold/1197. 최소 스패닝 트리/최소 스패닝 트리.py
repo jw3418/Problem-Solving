2edd0e165b024fd01ad2fb701ed12c2f42a6a2c5
ewillwin <jw3418@naver.com>
2025-03-04 13:59:57 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 264 ms, Memory: 57288 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,29 +1,24 @@
 import sys
+sys.setrecursionlimit(10 ** 6)
+input = sys.stdin.readline
 
-def find_parent(parent, x):
-    if parent[x] != x:
-        parent[x] = find_parent(parent, parent[x])
+V, E = map(int, input().split())
+edges = [list(map(int, input().split())) for e in range(E)]
+edges.sort(key=lambda x:x[2])
+
+def find(x):
+    if parent[x] != x: parent[x] = find(parent[x])
     return parent[x]
 
-def union_parent(parent, a, b):
-    a = find_parent(parent, a)
-    b = find_parent(parent, b)
+def union(a, b):
+    a = find(a); b = find(b)
     if a < b: parent[b] = a
     else: parent[a] = b
 
-V, E = map(int, sys.stdin.readline()[:-1].split())
-edges = []
-for e in range(E):
-    A, B, C = map(int, sys.stdin.readline()[:-1].split())
-    edges.append((A, B, C))
-edges.sort(key=lambda x:x[2])
-
-parent = [i for i in range(V+1)]
-
-total_cost = 0
-for edge in edges:
-    a, b, cost = edge
-    if find_parent(parent, a) != find_parent(parent, b):
-        total_cost += cost
-        union_parent(parent, a, b)
-print(total_cost)
+parent = [v for v in range(V+1)]
+ans = 0
+for u, v, w in edges:
+    if find(u) != find(v):
+        ans += w
+        union(u, v)
+print(ans)
