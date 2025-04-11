a5cf868d774ec559476d22347fe9b434692dfa82
ewillwin <jw3418@naver.com>
2025-04-12 01:45:15 +0900

    [level 2] Title: 의상, Time: 0.07 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=cpp) 
 
 ### 성능 요약
 
-메모리: 9.14 MB, 시간: 0.01 ms
+메모리: 4.14 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 02일 22:09:58
+2025년 04월 12일 01:45:12
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+    
+    unordered_map<string, vector<string>> map_;
+    
+    for (auto it = clothes.begin(); it < clothes.end(); ++it)
+    {
+        map_[(*it)[1]].push_back((*it)[0]);
+    }
+    
+    for (const auto &map : map_)
+    {
+        for (int i = 0; i < map.second.size(); i++)
+        {
+            cout << map.second[i] << " ";
+        }
+        cout << endl;
+    }
+    
+    int answer = 1;
+    for (const auto &map : map_)
+    {
+        answer *= (map.second.size() + 1);
+    }
+    answer -= 1;
+    return answer;
+    
+    
+}
