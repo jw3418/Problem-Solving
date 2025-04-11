4578fe0df4bef82a74ecb6df5da922a92e55826c
ewillwin <jw3418@naver.com>
2025-04-11 23:32:27 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 74.21 ms, Memory: 25.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -2,36 +2,50 @@
 #include <vector>
 #include <map>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 string solution(vector<string> participant, vector<string> completion) {
     
-    map<string, int> Cnt;
-    for (auto it = completion.begin(); it != completion.end(); ++it)
-    {
-        if (Cnt.find(*it) != Cnt.end())
-        {
-            Cnt[*it] += 1;
-        }
-        else
-        {
-            Cnt[*it] = 1;
-        }
-    }
+//     map<string, int> Cnt;
+//     for (auto it = completion.begin(); it != completion.end(); ++it)
+//     {
+//         if (Cnt.find(*it) != Cnt.end())
+//         {
+//             Cnt[*it] += 1;
+//         }
+//         else
+//         {
+//             Cnt[*it] = 1;
+//         }
+//     }
+    
+//     string answer = "";
+//     for (auto &name : participant)
+//     {
+//         if ((Cnt.find(name) != Cnt.end()) && (Cnt[name] != 0)){
+//             Cnt[name] -= 1;
+//         }
+//         else{
+//             answer += name;
+//             break;
+//         }
+//     }
     
     string answer = "";
-    for (auto &name : participant)
+    
+    sort(participant.begin(), participant.end());
+    sort(completion.begin(), completion.end());
+    
+    for (int i = 0; i < completion.size(); i++)
     {
-        if ((Cnt.find(name) != Cnt.end()) && (Cnt[name] != 0)){
-            Cnt[name] -= 1;
-        }
-        else{
-            answer += name;
-            break;
+        if (participant[i] != completion[i])
+        {
+            return participant[i];
         }
     }
     
-    return answer;
+    return participant[completion.size()];
 }
             
