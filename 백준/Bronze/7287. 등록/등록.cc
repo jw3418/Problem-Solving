c58e24609103374743200f1a299eae30ae3c0ddb
ewillwin <jw3418@naver.com>
2025-04-19 15:38:50 +0900

    [Bronze V] Title: 등록, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+	cout << "670" << endl;
+	cout << "jw3418";
+
+	return 0;
+}
