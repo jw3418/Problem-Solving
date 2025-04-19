0493107cecce23d9ffca3ec7dcc38a03e813a0fc
ewillwin <jw3418@naver.com>
2025-04-19 15:31:41 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+	int A, B, C, D, E, F;
+	cin >> A >> B >> C >> D >> E >> F;
+	cout << 1-A << " " << 1-B << " " << 2-C << " " << 2-D << " " << 2-E << " " << 8-F;
+
+	return 0;
+}
