7dff9b67033067c5ee375cf02a7590c678d8dc86
ewillwin <jw3418@naver.com>
2025-04-21 01:11:52 +0900

    [Bronze V] Title: 오늘 날짜, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+	
+	cout << "2025-04-21";
+
+	return 0;
+}
