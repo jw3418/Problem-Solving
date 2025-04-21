450a4af811f59384490ee3a9125807c462c8c97c
ewillwin <jw3418@naver.com>
2025-04-21 21:22:09 +0900

    [Silver V] Title: 올림픽, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.cc
+++ 백준/Silver/8979. 올림픽/올림픽.cc
@@ -17,16 +17,21 @@ int main()
 		state.push_back({g, s, b, n});
 	}
 
-	sort(state.begin(), state.end(), greater<>());
+	sort(state.begin(), state.end(), [](const vector<int> &a, const vector<int> &b)
+	{
+		if (a[0] != b[0]) return a[0] > b[0];
+		if (a[1] != b[1]) return a[1] > b[1];
+		return a[2] > b[2];
+	});
 
-	int gg, ss, bb; gg = state[0][0]; ss = state[0][1]; bb = state[0][2];
+	int gg = state[0][0], ss = state[0][1], bb = state[0][2];
 	int ans = 1;
 	for (int i = 0; i < N; ++i)
 	{
 		if (gg != state[i][0] || ss != state[i][1] || bb != state[i][2])
 		{
 			gg = state[i][0]; ss = state[i][1]; bb = state[i][2];
-			ans++;
+			ans = i+1;
 		}
 		if (state[i][3] == K)
 		{
