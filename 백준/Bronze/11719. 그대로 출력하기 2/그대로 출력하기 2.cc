082715fe0db3284cfcfaf28272dab161eb924534
ewillwin <jw3418@naver.com>
2025-04-23 23:42:34 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
+#include <map>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string line;
+	while(getline(cin, line))
+	{
+		cout << line << endl;
+	}
+
+    return 0;
+}
